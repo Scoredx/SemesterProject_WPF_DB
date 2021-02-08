c5f5eda61929ff359a12dae1bf2c49e960e46e4d
Scoredx <multiusage2@o2.pl>
2021-02-08 16:58:29 +0100

    Inesrt Values in Table

---
+++ SQL/createTables.sql
@@ -0,0 +1,57 @@
+﻿create table addressTable(
+address_id int primary key NOT NULL,
+address_city varchar (40) NOT NULL,
+address_street varchar (40) NOT NULL,
+address_home_no int NOT NULL,
+address_flat_no int,
+);
+
+create table worker(
+worker_id int primary key NOT NULL,
+worker_pesel nchar(11) unique NOT NULL,
+worker_name varchar (15) NOT NULL,
+worker_surename varchar (30) NOT NULL,
+);
+
+create table category(
+category_id int primary key NOT NULL,
+category_name varchar (20) 
+);
+
+create table manufacturer(
+manufacturer_id int primary key NOT NULL,
+manufacturer_name varchar(40) NOT NULL
+);
+
+create table delivery_type(
+delivery_type_id int primary key NOT NULL,
+delivery_type varchar(20) NOT NULL,
+delivery_cost money NOT NULL,
+);
+
+create table customer(
+customer_id int primary key NOT NULL,
+customer_name varchar (30) NOT NULL,
+customer_surename varchar (30) NOT NULL,
+customer_address_id int foreign key references addressTable(address_id) NOT NULL,
+customer_phone int NOT NULL,
+customer_email varchar (25),
+customer_nip nchar(9) null
+);
+
+create table product(
+product_id int primary key NOT NULL,
+product_name varchar (40) NOT NULL,
+product_manufacturer int foreign key references manufacturer(manufacturer_id) NOT NULL,
+product_category_id int foreign key references category(category_id) NOT NULL,
+product_price money NOT NULL,
+product_cost money NOT NULL,
+);
+
+create table orderTable(
+order_id int primary key,
+order_product_id int foreign key references product(product_id) NOT NULL,
+order_delivery_type_id int foreign key references delivery_type(delivery_type_id) NOT NULL,
+order_customer_id int foreign key references customer(customer_id) NOT NULL,
+order_date datetime NOT NULL,
+);
---
+++ SQL/insertInto.sql
@@ -0,0 +1,44 @@
+﻿insert into addressTable VALUES
+(1,'Heights','Cartonas',1),
+(2,'Brooklawn','Sarsat',1,5),
+(3,'Tanners','New York',1,9),
+(4,'Brooklawn','Sarsat',1),
+(5,'Lister','Balminster',1,15);
+
+insert into worker VALUES
+(1,72070182397,'Jackob','Choppa'),
+(2,73052858129,'Mary','Parks'),
+(3,00272552928,'Ruth','Simon'),
+(4,73051026413,'Leo','Potter');
+
+insert into category VALUES
+(1,'3D_FDM_Printer'),
+(2,'Thermistor'),
+(3,'3D_MSLA_Printer'),
+(4,'Resin'),
+(5,'Stepper_Motor');
+
+insert into manufacturer VALUES
+(1,'Prusa'),
+(2,'Trianglelabs'),
+(3,'Eachine'),
+(4,'Zortrax'),
+(5,'Anycubic');
+
+insert into delivery_type VALUES
+(1,'Personal PickUp',0),
+(2,'Courier',10),
+(3,'Parcel post',4);
+
+insert into customer VALUES
+(1,'Keagan','Hilton',1,111222333),
+(2,'Matas','Cresswell',3,333444555,999999999);
+
+insert into product VALUES
+(1,'Mini',4,1,1000,800),
+(2,'MK3s',1,1,500,340);
+
+insert into orderTable VALUES
+(1,1,3,1,'2020-01-11'),
+(2,2,1,2,'2021-01-02');
+
